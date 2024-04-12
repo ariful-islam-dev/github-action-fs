cda27a77df2ed2f10e70995646221e96ee7cfb5d
ariful4082 <ariful4082@gmail.com>
2024-04-12 22:05:15 +0600

    feat: added delivery workflow

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.get("/",(req, res)=>{
     res.status(201).send({message: "Hello World"});
 })
 
-app.use((req, res, next)=>{
-    res.status(404).send({message: "Page Not Found"})
-})
+// app.use((req, res, next)=>{
+//     res.status(404).send({message: "Page Not Found"})
+// })
 
 
 module.exports = app;
