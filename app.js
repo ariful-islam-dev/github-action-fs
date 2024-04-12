17d6858d2ddebcab795dfa15066616a50ab0b2a6
ariful4082 <ariful4082@gmail.com>
2024-04-12 22:14:25 +0600

    fix:test application file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.get("/",(req, res)=>{
-    res.status(200).send({message: "Hello World"});
+    res.status(200).send("Hello World");
 })
 
 // app.use((req, res, next)=>{
